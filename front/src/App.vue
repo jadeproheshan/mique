389928e5ad95c3fb95fc0ddd7133eea95c7c9640
Linkingme <wyzhhxygyxqhhy@163.com>
2025-06-27 08:58:52 +0800

    0627App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -4,6 +4,7 @@
     <a-layout-header
       style="background: #fff; height: 64px; line-height: 64px; box-shadow: 0 2px 8px #f0f1f2; position: fixed; top: 0; left: 0; width: 100%; z-index: 10;"
     >
+
       <div style="font-size: 20px; font-weight: bold; color: #1677ff;">
         河南大学社团管理系统
       </div>
@@ -61,7 +62,7 @@
           <div style="padding: 24px; background: #fff; text-align: center;">
 
             //page-header
-            <div class="page-header" style="height:160px;background-color: aquamarine;">
+            <div class="page-header" style="height:160px;">
               <div style="font-size: 64px; font-weight: bold;">社团名称</div>
               <div style="font-size: 24px;">成员人数、指导老师、挂靠单位</div>
             </div>
@@ -96,7 +97,7 @@
                 <DownloadOutlined />下载</a-button>
             </a-space>
 
-            //高级搜索下-成员列表页 （有script）
+            //高级搜索下-成员列表页
             <a-table
               :columns="columns"
               :row-key="record => record.login.uuid"
@@ -108,7 +109,96 @@
               <template #name="{ text }">{{ text.first }} {{ text.last }}</template>
             </a-table>
             <script lang="ts">
-            
+            import { TableState, TableStateFilters } from 'ant-design-vue/es/table/interface';
+            import { usePagination } from 'vue-request';
+            import { computed, defineComponent } from 'vue';
+            const columns = [
+              {
+                title: '姓名',
+                dataIndex: 'name',
+                sorter: true,
+                width: '20%',
+                slots: { customRender: 'name' },
+              },
+              {
+                title: '部门',
+                dataIndex: 'department',
+                width: '20%',
+              },
+              {
+                title: '学/工号',
+                dataIndex: 'studentId',
+              },
+              {
+                title: '手机号',
+                dataIndex: 'phonenumber',
+                width: '20%',
+              },
+              {
+                title: '还有别的栏 没写完',
+                dataIndex: 'other',
+                width: '20%',
+              },
+            ];
+
+            type Pagination = TableState['pagination'];
+            type APIParams = {
+              results: number;
+              page?: number;
+              sortField?: string;
+              sortOrder?: number;
+              [key: string]: any;
+            };
+            type APIResult = {
+              results: {
+                gender: 'female' | 'male';
+                name: string;
+                email: string;
+              }[];
+            };
+
+            const queryData = (params: APIParams) => {
+              return `https://randomuser.me/api?noinfo&${new URLSearchParams(params)}`;
+            };
+
+            export default defineComponent({
+              setup() {
+                const { data: dataSource, run, loading, current, pageSize } = usePagination<APIResult>(
+                  queryData,
+                  {
+                    formatResult: res => res.results,
+                    pagination: {
+                      currentKey: 'page',
+                      pageSizeKey: 'results',
+                    },
+                  },
+                );
+
+                const pagination = computed(() => ({
+                  total: 200,
+                  current: current.value,
+                  pageSize: pageSize.value,
+                }));
+
+                const handleTableChange = (pag: Pagination, filters: TableStateFilters, sorter: any) => {
+                  run({
+                    results: pag!.pageSize!,
+                    page: pag?.current,
+                    sortField: sorter.field,
+                    sortOrder: sorter.order,
+                    ...filters,
+                  });
+                };
+
+                return {
+                  dataSource,
+                  pagination,
+                  loading,
+                  columns,
+                  handleTableChange,
+                };
+              },
+            });
             </script>
 
             //留白（看看还需要么？）
@@ -143,8 +233,6 @@ const selectedKeys = ref<string[]>(['4']);
 </script>
 
 <style scoped>
-
-//tab与高级搜索
 .manager-tab{
   .ant-tabs-tab{
     font-size:16px;
